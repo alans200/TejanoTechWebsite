b27fafda2b90de86ee13b6931b60d2b8d028bef5
alans200 <asaen200@my.epcc.edu>
2023-04-13 14:44:05 -0600

    Github Pages Test

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static(__dirname + "/public"))
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
